8a4a073b959bc048f053e8cff7e192f2c67073b3
sr-sohan15 <sr.sohan7815@gmail.com>
2026-08-14 02:07:08 +0600

    fix: remove unused BookDetail route and update basename

--- src/Routes/Routes.jsx
+++ src/Routes/Routes.jsx
@@ -4,22 +4,22 @@ import Roots from "../Pages/Roots/Roots";
 import ErrorPage from "../Pages/ErrorPage/ErrorPage";
 import Home from "../Pages/Home/Home";
 
-export const router = createBrowserRouter([
+export const router = createBrowserRouter(
+  [
+    {
+      path: "/",
+      element: <Roots />,
+      errorElement: <ErrorPage />,
+      children: [
+        {
+          index: true,
+          loader: () => fetch('./booksData.json'), 
+          element: <Home />,
+        },
+      ],
+    },
+  ],
   {
-    path: "/",
-    Component: Roots,
-    errorElement: <ErrorPage />,
-    HydrateFallback: () => (
-      <div className="flex justify-center items-center my-20">
-        <span className="loading loading-spinner loading-lg text-primary"></span>
-      </div>
-    ),
-    children: [
-      {
-        index: true,
-        loader: () => fetch('/booksData.json'),
-        Component: Home,
-      },
-    ],
-  },
-]);
+    basename: "/B11-Boi-poka",
+  }
+);
--- vite.config.js
+++ vite.config.js
@@ -2,9 +2,9 @@ import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
 
-
 export default defineConfig({
   plugins: [react(), tailwindcss()],
+  base: '/B11-Boi-poka/',
   server: {
     watch: {
       usePolling: true, 
